c30a47f3c452086694790cb21da6684e4602f5ff
Severin Friede <sevefriede@gmail.com>
2017-12-22 14:19:37 +0100

    basic features added

--- README.md
+++ README.md
@@ -1,16 +1,13 @@
-# ngx-translate-zombies README
+# ngx-translate-zombies
 
-This is the README for your extension "ngx-translate-zombies". After writing up a brief description, we recommend including the following sections.
+This extension should be used withe angular translation library [ngx-translate](https://github.com/ngx-translate/core)
+It searches translation keys (normally located in a [language-code].json file) in .ts, .js or .html files
+in the current workspace and shows unused keys inside a diff editor.
 
 ## Features
 
-Describe specific features of your extension including screenshots of your extension in action. Image paths are relative to this README file.
-
-For example if there is an image subfolder under your extension project workspace:
-
-\!\[feature X\]\(images/feature-x.png\)
-
-> Tip: Many popular extensions utilize animations. This is an excellent way to show off your extension! We recommend short, focused animations that are easy to follow.
+When having a ngx-translate translation file opened, you can hit F1 and type ```ngxtz: find translation zombies in this file```.
+A diff editor will be openend showing the recommended changes. That's it.
 
 ## Requirements
 
@@ -18,48 +15,13 @@ If you have any requirements or dependencies, add a section describing those and
 
 ## Extension Settings
 
-Include if your extension adds any VS Code settings through the `contributes.configuration` extension point.
-
-For example:
-
-This extension contributes the following settings:
-
-* `myExtension.enable`: enable/disable this extension
-* `myExtension.thing`: set to `blah` to do something
+No settings yet.
 
 ## Known Issues
 
-Calling out known issues can help limit users opening duplicate issues against your extension.
+No issues yet.
 
 ## Release Notes
 
-Users appreciate release notes as you update your extension.
-
-### 1.0.0
-
-Initial release of ...
-
-### 1.0.1
-
-Fixed issue #.
-
-### 1.1.0
-
-Added features X, Y, and Z.
-
------------------------------------------------------------------------------------------------------------
-
-## Working with Markdown
-
-**Note:** You can author your README using Visual Studio Code.  Here are some useful editor keyboard shortcuts:
-
-* Split the editor (`Cmd+\` on OSX or `Ctrl+\` on Windows and Linux)
-* Toggle preview (`Shift+CMD+V` on OSX or `Shift+Ctrl+V` on Windows and Linux)
-* Press `Ctrl+Space` (Windows, Linux) or `Cmd+Space` (OSX) to see a list of Markdown snippets
-
-### For more information
-
-* [Visual Studio Code's Markdown Support](http://code.visualstudio.com/docs/languages/markdown)
-* [Markdown Syntax Reference](https://help.github.com/articles/markdown-basics/)
-
-**Enjoy!**
+### 0.0.1
+Basis features added (searching zombies, showing diff)
--- src/extension.ts
+++ src/extension.ts
@@ -1,29 +1,115 @@
 'use strict';
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
+import * as fs from 'fs';
 import * as vscode from 'vscode';
+import { Range } from 'vscode';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "ngx-translate-zombies" is now active!');
+  let disposable = vscode.commands.registerCommand('ngxtz.check', () => {
+    const currentFileName = vscode.window.activeTextEditor.document.fileName;
+    if (!currentFileName.endsWith('.json')) {
+      vscode.window.showWarningMessage('Expected a JSON file.');
+      return;
+    }
 
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with  registerCommand
-    // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('extension.sayHello', () => {
-        // The code you place here will be executed every time your command is executed
+    if (vscode.workspace.name === undefined) {
+      vscode.window.showWarningMessage('This extension must be used in a workspace.');
+      return;
+    }
 
-        // Display a message box to the user
-        vscode.window.showInformationMessage('Hello World!');
-    });
+    try {
+      const langDoc = vscode.window.activeTextEditor.document;
+      const text = langDoc.getText();
+      const json = JSON.parse(text);
+      const keys = getTranslationKeys(json, null, []);
+      vscode.workspace.findFiles('**/*.{ts,js,html}', '**/node_modules/**')
+        .then((files) => {
+          let zombies = [ ...keys ];
+          for (let index = 0; index < files.length; index++) {
+            const file = files[index];
+            zombies = findInFile(file, zombies);
+          }
 
-    context.subscriptions.push(disposable);
+          let unzombified = { ...json };
+          for (let index = 0; index < zombies.length; index++) {
+            const zombie = zombies[index];
+            const zombieKeys = zombie.split('.');
+            const prop = zombieKeys.pop();
+            const parent = zombieKeys.reduce((obj, key) => obj[key], unzombified);
+            delete parent[prop];
+          }
+          const content = JSON.stringify(unzombified, null, 2);
+          vscode.workspace.openTextDocument({ content })
+            .then(
+              doc => {
+                const fileName = langDoc.fileName.replace(vscode.workspace.rootPath, '').substring(1);
+                return vscode.commands.executeCommand(
+                  'vscode.diff',
+                  langDoc.uri,
+                  doc.uri,
+                  `${fileName} ↔ unzombified`
+                )
+                .then(
+                  ok => {
+                    console.log('done');
+                  },
+                  err => {
+                    const errorMessage = 'Error opening diff editor.';
+                    vscode.window.showErrorMessage(errorMessage);
+                    console.error(errorMessage, err);
+                  }
+                );
+              },
+              err => {
+                const errorMessage = 'Error opening temporary file.';
+                vscode.window.showErrorMessage(errorMessage);
+                console.error(errorMessage, err);
+              }
+            );
+        });
+    } catch (err) {
+      const errorMessage = 'Error while parsing the file: ' + currentFileName;
+      vscode.window.showErrorMessage(errorMessage);
+      console.error(errorMessage, err);
+    }
+  });
+
+  context.subscriptions.push(disposable);
 }
 
-// this method is called when your extension is deactivated
+function findInFile(uri: vscode.Uri, keys: string[]): string[] {
+  const zombies = [ ...keys ];
+  try {
+    const data = fs.readFileSync(uri.fsPath);
+    for (const key of keys) {
+      const found = data.indexOf(key) !== -1;
+      const zombieIndex = zombies.indexOf(key);
+      const alreadyZombie = zombieIndex !== -1;
+      if (!found && !alreadyZombie) {
+        zombies.push(key);
+      }
+      if (found && alreadyZombie) {
+        zombies.splice(zombieIndex, 1);
+      }
+    }
+  } catch (err) {
+    console.error('error while reading file: ' + uri.fsPath, err);
+  }
+  return zombies;
+}
+
+function getTranslationKeys(obj: Object, cat: string, tKeys: string[]): string[] {
+  const currentKeys = [ ...tKeys ];
+  for (const key of Object.keys(obj)) {
+    const value = obj[key];
+    if (typeof value === 'object') {
+      currentKeys.push(...getTranslationKeys(value, cat === null ? key : cat.concat('.', key), tKeys));
+    } else {
+      currentKeys.push(cat === null ? key : cat.concat('.', key));
+    }
+  }
+  return currentKeys;
+}
+
 export function deactivate() {
 }
